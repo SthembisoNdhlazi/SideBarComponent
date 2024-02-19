9b341b66f11a34b0ebd67b3180d184eca29ccae5
Sthembiso Ndhlazi <101163890+SthembisoNdhlazi@users.noreply.github.com>
2024-02-19 15:11:33 +0200

    Add navigationStack

--- Sources/SideBarComponent/SideBarComponent.swift
+++ Sources/SideBarComponent/SideBarComponent.swift
@@ -10,6 +10,7 @@ public struct SidebarComponent<Provider: SideBarConfigurable>: View {
     @StateObject public var dataProvider: Provider
     @State var selectedView: AnyView = AnyView(EmptyView())
     @State var index = 0
+    @State var navPath = NavigationPath()
     var title: String
     
     public init(dataProvider: Provider, title: String) {
@@ -18,60 +19,63 @@ public struct SidebarComponent<Provider: SideBarConfigurable>: View {
     }
     
     public var body: some View {
-        
-        HStack(spacing: 0) {
-            
-            VStack {
-                Group {
-                    
-                    ForEach(Array((dataProvider.categories.enumerated())), id: \.offset) { index, category in
-                        Button(action: {
-                            self.index = index
-                            self.selectedView = category.view
-                        }) {
-                            VStack {
-                                Text(category.category)
-                                    .frame(width: 120,height: 35)
-                                    .foregroundColor(self.index == index ? Color.black : Color.gray)
-                                    .underline(self.index == index)
-                            }
-                        }
-                        .rotationEffect(.init(degrees: -90))
-                        .padding(.top, 80)
-                    }
-                }
-                .padding(.leading, 15)
-            }
-            .padding(.vertical)
-            .frame(width: 50)
-            .background(Color.clear)
-            .onFirstAppear {
-                if let displayView = dataProvider.categories.last?.view {
-                    index = dataProvider.categories.count - 1
-                    selectedView = displayView
-                } else {
-                    selectedView = AnyView(EmptyView())
-                }
-            }
-            
-            GeometryReader { reader in
-                
-                HStack {
-                    Spacer(minLength: reader.size.width * 0.1)
-                    Text(title)
-                        .font(.system(size: 20,weight: .bold))
-                    Spacer(minLength: reader.size.width * 0.45)
-                }
-                .padding(.top, 60)
+        NavigationStack(path: $navPath) {
+            HStack(spacing: 0) {
                 
                 VStack {
-                    selectedView
-//                        .padding(.trailing, 5)
+                    Group {
+                        
+                        ForEach(Array((dataProvider.categories.enumerated())), id: \.offset) { index, category in
+                            Button(action: {
+                                self.index = index
+                                self.selectedView = category.view
+                            }) {
+                                VStack {
+                                    Text(category.category)
+                                        .frame(width: 120,height: 35)
+                                        .foregroundColor(self.index == index ? Color.black : Color.gray)
+                                        .underline(self.index == index)
+                                }
+                            }
+                            .rotationEffect(.init(degrees: -90))
+                            .padding(.top, 80)
+                        }
+                    }
+                    .padding(.leading, 15)
                 }
-                .padding(.top, 100)
+                .padding(.vertical)
+                .frame(width: 50)
+                .background(Color.clear)
+                .onFirstAppear {
+                    if let displayView = dataProvider.categories.last?.view {
+                        index = dataProvider.categories.count - 1
+                        selectedView = displayView
+                    } else {
+                        selectedView = AnyView(EmptyView())
+                    }
+                }
+                
+                GeometryReader { reader in
+                    
+                    HStack {
+                        Spacer(minLength: reader.size.width * 0.1)
+                        Text(title)
+                            .font(.system(size: 20,weight: .bold))
+                        Spacer(minLength: reader.size.width * 0.45)
+                    }
+                    .padding(.top, 60)
+                    
+                    VStack {
+                        selectedView
+                        //                        .padding(.trailing, 5)
+                    }
+                    .padding(.top, 100)
+                }
             }
+            .padding(.bottom)
+            .edgesIgnoringSafeArea(.all)
         }
-        .edgesIgnoringSafeArea(.all)
+        .accentColor(.black)
     }
 }
 
